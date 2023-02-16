9ce72913f9273984dc550c0772ea687d15fdd671
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2023-02-16 20:29:36 +0100

    milestone 3 + 4 + 5

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <script>
-export default {
+import TheCard from "./components/TheCard.vue";
+import axios from "axios";
 
+export default {
+  components: { TheCard },
+
+  data() {
+    return {
+      backendUrl: "http://127.0.0.1:8000",
+      projects: []
+    };
+  },
+
+  methods: {
+    fetchProjects() {
+      axios.get("http://127.0.0.1:8000/api/projects")
+        .then((resp) => {
+          console.log(resp.data);
+          this.projects = resp.data;
+        });
+
+    }
+  },
+
+  mounted() {
+    this.fetchProjects();
+  }
 }
 </script>
 
 <template>
+  <div class="container">
+    <div>
+      <h1>I tuoi progetti</h1>
+    </div>
 
+    <TheCard :projects="projects"></TheCard>
+  </div>
 </template>
 
 <style lang="scss">
---
+++ src/components/TheCard.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    props: {
+        projects: Object
+    },
+}
+</script>
+
+<template>
+    <div class="row g-3">
+        <div class="col-3" v-for="project in projects" :key="project.id">
+            <div class="card text-center" style="height: 350px">
+                <div class="card-img-top">
+                    <img  class="img-fluid" :src="`http://127.0.0.1:8000/storage/${project.cover_img}`" alt=""/>
+                    <!-- <span>{{ project.cover_img }}</span> -->
+                </div>
+                <div class="card-title">
+                    <h2>{{ project.name }}</h2>
+                </div>
+                <div class="card-text">
+                    <span>description: </span>
+                    <span>{{ project.description.slice(0, 20) }}...</span>
+                    <br>
+                    <span>github: </span>
+                    <span class="d-block">{{ project.github_link }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
