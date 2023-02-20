bb93114f399dff0e66a709680860007c48019808
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2023-02-20 17:55:47 +0100

    collegamento al database

--- src/pages/Contatti.vue
+++ src/pages/Contatti.vue
@@ -6,6 +6,8 @@ export default {
     name: 'Contatti',
     data() {
         return {
+            loading: false,
+            submitResult: "",
             form: {
                 title: "",
                 email: "",
@@ -13,42 +15,66 @@ export default {
                 attachment: "",
             },
         }
-    }
+    },
+    methods: {
+        onFormSubmit() {
+            this.loading = true;
+            axios.post(store.backendUrl + "/api/contacts", this.form)
+                .then(resp => {
+                    this.submitResult = "success";
+                    this.loading = false;
+                })
+                .catch((e) => {
+                    if (e.response && e.response.data) {
+                        this.submitResult = e.response.data.message;
+                    } else {
+                        this.submitResult = e.message;
+                    }
+
+                    this.loading = false;
+                });
+        },
+    },
 }
 </script>
 
 <template>
-<div class="container">
-    <h1>Pagina Contatti</h1>
-    <form>
-        <div class="mb-3">
-            <label for="titleInput" class="form-label">Titolo</label>
-            <input type="text" class="form-control" id="titleInput" v-model="form.title" />
+    <div class="container">
+        <h1>Pagina Contatti</h1>
+        <div class="alert alert-success mt-5" v-if="submitResult === 'success'">
+            <span>Form inviato correttamente. Ti risponderemo il prima possibile</span>
         </div>
-    
-        <div class="mb-3">
-            <label for="emailInput" class="form-label">Email</label>
-            <input type="email" class="form-control" id="emailInput" v-model="form.email" />
+        <div class="alert alert-danger mt-5" v-else-if="submitResult">
+            <span> Sembra che ci sia stato un errore. Ti invitiamo a riprovare pià tardi.<br>
+                {{ submitResult }}</span>
         </div>
-    
-        <div class="mb-3">
-            <label for="messageInput" class="form-label">Messaggio</label>
-            <textarea class="form-control" id="messageInput" v-model="form.message"></textarea>
-        </div>
-    
-        <div class="mb-3">
-            <label for="attachmentInput" class="form-label">Allegato</label>
-            <input type="file" class="form-control" id="attachmentInput"/>
-        </div>
-    
-        <div class="d-flex justify-content-center gap-3">
-            <button class="btn btn-secondary" typeof="reset">
-                Annulla
-            </button>
-            <button class="btn btn-success"> Invia </button>
-        </div>
-    </form>
-</div>
+        <form @submit.prevent="onFormSubmit" v-if="submitResult !=='success'" >
+            <div class="mb-3">
+                <label for="titleInput" class="form-label">Titolo</label>
+                <input type="text" class="form-control" id="titleInput" v-model="form.title" />
+            </div>
+
+            <div class="mb-3">
+                <label for="emailInput" class="form-label">Email</label>
+                <input type="email" class="form-control" id="emailInput" v-model="form.email" />
+            </div>
+
+            <div class="mb-3">
+                <label for="messageInput" class="form-label">Messaggio</label>
+                <textarea class="form-control" id="messageInput" v-model="form.message"></textarea>
+            </div>
+
+            <div class="mb-3">
+                <label for="attachmentInput" class="form-label">Allegato</label>
+                <input type="file" class="form-control" id="attachmentInput" />
+            </div>
+
+            <div class="d-flex justify-content-center gap-3">
+                <button class="btn btn-secondary" typeof="reset"> Annulla </button>
+                <button class="btn btn-success"> Invia </button>
+            </div>
+        </form>
+    </div>
 </template>
 
 <style scoped lang="scss">
